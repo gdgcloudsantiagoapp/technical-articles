7f472f70d97fd9219615f24f4bdfc5427e845c25
Felipe Andrés Velásquez Castro <felipe.velasquezc@gmail.com>
2020-10-03 17:29:56 -0300

    Docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,94 @@
+module.exports = {
+  title: "Cloud Articles Repo",
+  tagline: "Articulos y documentos de la comunidad GDG Cloud santiago",
+  url: "https://cloud-docs.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "ignore",
+  favicon: "img/favicon.ico",
+  organizationName: "cloud-docs",
+  projectName: "cloud-docs.github.io",
+  themeConfig: {
+    sidebarCollapsible: false,
+    navbar: {
+      title: 'GDG Cloud Santiago',
+      logo: {
+        alt: 'GDG Cloud Santiago',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
